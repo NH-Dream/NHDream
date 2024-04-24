3a27bb4af0b32240c051b52d3e8fa7537730b009
권현준 <bonheur9813@naver.com>
2024-04-24 21:19:45 +0900

    🎉 : 백엔드 프로젝트 생성
    Former-commit-id: 7ef065dba449319ce573e1e9c023682fbe342744

---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/NhdreamApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.nhdream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhdreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhdreamApplication.class, args);
+	}
+
+}
