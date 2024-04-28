bd99cd8db10174a4c9bfcff56459cfcacdaacca9
김준엽 <1025jy@naver.com>
2024-04-28 14:16:19 +0900

    ✨: AwsS3Service 추가
    Former-commit-id: 85a8d6189dcc529fde2eb106f388d17b6faef67e

--- backend/nhdream/src/main/java/com/ssafy/nhdream/common/s3/AwsS3Service.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/common/s3/AwsS3Service.java
@@ -1,4 +1,3 @@
-package com.ssafy.nhdream.common.s3;
-
-public class AwsS3Service {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ff47f679ed9b042f8983cfac7611a08e724f45811d1272d9a957400ac9ac516e
+size 4611
