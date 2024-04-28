116757ff905a3b123c2fa804109617c93f947879
김준엽 <wnsduq1025@gmail.com>
2024-04-28 14:09:35 +0900

    ✨: AwsS3Config 추가
    Former-commit-id: 0c2c6d575648a38a6fc6c06bd47aee3996181cfd

--- backend/nhdream/src/main/java/com/ssafy/nhdream/common/config/AwsS3Config.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/common/config/AwsS3Config.java
@@ -1,4 +1,3 @@
-package com.ssafy.nhdream.common.config;
-
-public class AwsS3Config {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b70904b04a561b3c8fff4c63e3684862a78f99dbb022c753322845e1b44b46f7
+size 1222
