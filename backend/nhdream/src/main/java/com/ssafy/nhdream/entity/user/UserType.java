36d490ec6cf6533a3c3fc60571b7916c9a2f5a3e
권현준 <bonheur9813@naver.com>
2024-04-30 03:40:30 +0900

    📝 : User 엔티티 파일 작성
    - User 엔티티 / UserType Enum 클래스/ Wallet 엔티티 작성
    
    Related to : S10P31S209-97
    
    
    Former-commit-id: d7bd52684f343f6fa9da3e52756e4db0fd2effb8

---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/user/UserType.java
@@ -0,0 +1,7 @@
+package com.ssafy.nhdream.entity.user;
+
+public enum UserType {
+    UNVERIFIED, // 미인증
+    TRAINEE,    // 교육생
+    FARMER // 귀농
+}
---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/user/Wallet.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:725a66ef5c79c7600b54dfc81776e57f70a29222340af5e42c2fad474fea0bfd
+size 664
