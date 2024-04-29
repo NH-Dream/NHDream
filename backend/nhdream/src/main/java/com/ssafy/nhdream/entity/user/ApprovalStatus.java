1a48dedf8d5f7a6f6995a23cb1bb3ae5a86594ea
권현준 <bonheur9813@naver.com>
2024-04-30 03:46:54 +0900

    📝 : 승인 관련 엔티티 작성
    - 승인 상태 Enum 작성
    - 교육인증을 위한 Education 작성
    - 귀농인증을 위한 Certified 작성
    
    Related to : S10P31S209-97
    
    
    Former-commit-id: 2c6c26d7705076dc2bf935f4daaba1ab468afed7

---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/user/ApprovalStatus.java
@@ -0,0 +1,7 @@
+package com.ssafy.nhdream.entity.user;
+
+public enum ApprovalStatus {
+    PENDING,    // 대기
+    APPROVED,   // 승인
+    REJECTED    // 반려
+}
---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/user/Education.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3168d1f0809498ef68f1376ff58126c0110c417981363b3b934ddcdce9138b54
+size 1579
