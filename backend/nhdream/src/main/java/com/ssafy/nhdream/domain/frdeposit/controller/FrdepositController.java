5b8d9ced699eef9b67bf3919b6e10927e9623829
김준엽 <wnsduq1025@gmail.com>
2024-04-29 11:18:50 +0900

    ✨: deposit 추가
    Former-commit-id: 08bf807f6e7dde30c98a361db21b1e3514e521bf

---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/frdeposit/controller/FrdepositController.java
@@ -0,0 +1,4 @@
+package com.ssafy.nhdream.domain.frdeposit.controller;
+
+public class FrdepositController {
+}
---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/frdeposit/repository/FrdepositRepository.java
@@ -0,0 +1,4 @@
+package com.ssafy.nhdream.domain.frdeposit.repository;
+
+public interface FrdepositRepository {
+}
