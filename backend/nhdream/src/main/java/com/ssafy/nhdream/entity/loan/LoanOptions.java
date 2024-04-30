47dc7659f977f6d130acbfff728c124db431a87c
김준엽 <wnsduq1025@gmail.com>
2024-04-30 21:22:03 +0900

    ✨: LoanApproval 추가
    Former-commit-id: 127045a01808acf7471a12744349033f345d0a5f

--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanOptions.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanOptions.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d27a9180fbfebd618774acbf9d0cc4c22b314e48dd35cce0b72dd535b1ef894a
-size 1744
+oid sha256:8d4acf44253f2659c8270f049b84f304d770bf29afa822512a0c41b7e5b1a0a1
+size 1846
