eb88ba701d520410b1bf2d948b5a0f1beaeb9242
조현제 <abb6575@naver.com>
2024-04-30 16:20:14 +0900

    ✨ : 적금엔티티 기능 추가
    적금 엔티티 시리즈 추가
    
    
    Former-commit-id: 882d1b75212f514e9e19f1412f7448c474ef535d

---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/saving/SavingOptions.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b3c97079bc624c47823f36a62658abcbc268622e86e7c83af1ad09d27898fe9
+size 1741
---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/saving/SavingTransaction.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0bf6761e46f6f59f0f2d4448c83354c1cc17ca5ae1b91aab27f35a8b5825f962
+size 1672
