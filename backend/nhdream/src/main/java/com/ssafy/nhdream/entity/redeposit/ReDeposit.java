7f5ea0c1e407d6c3d6a317fdb6d70780ca0521c1
권현준 <bonheur9813@naver.com>
2024-05-01 15:25:37 +0900

    ✨ : 정기예금 엔티티 추가
    - ReDepositAccount / ReDepositOptions / ReDepositProduct / ReDepositTransaction 파일 작성
    - User 엔티티 정기예금 필드 추가
    
    Related to : S10P31S209-97
    
    
    Former-commit-id: 4f3b3c7ef036260d829d4755c0aaeab5c963e4ce

--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/redeposit/ReDeposit.java
+++
@@ -1,4 +0,0 @@
-package com.ssafy.nhdream.entity.redeposit;
-
-public class ReDeposit {
-}
---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/redeposit/ReDepositAccount.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:514ae6d15666d8ad3ba431762365bb293ca93144835edc3456a5c6a2304ed9c7
+size 2289
