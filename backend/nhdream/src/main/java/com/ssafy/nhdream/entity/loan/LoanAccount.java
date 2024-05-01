05ea70868ea0bd315ca12fcd73efdb2cdd077acf
김준엽 <1025jy@naver.com>
2024-05-01 15:53:47 +0900

    🔨: 매핑 추가를 위한 entity 주석 추가  수정
    Former-commit-id: 0c8bbf145e0805882d6e984daf58dc3df7626f46

--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanAccount.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanAccount.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bc3dff4b94da580b241cc349cdf8c7bbf0b9efd4ba1fb14f2e33660b12592ea
-size 3433
+oid sha256:2a2c6c5be4bfcc12b6d6f85d5a8a74ccd8e2498dd291372136b3816c7661afe3
+size 3500
--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanOptions.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanOptions.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d4acf44253f2659c8270f049b84f304d770bf29afa822512a0c41b7e5b1a0a1
-size 1846
+oid sha256:a0ef25ad57ab362df8c5c707dc8c7a62619f9de3d0cc79fbb71e26e79e0fb5f8
+size 1856
