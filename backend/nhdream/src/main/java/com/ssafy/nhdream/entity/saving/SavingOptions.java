d5123616da6a265d61f3d43319897c978f44814f
조현제 <abb6575@naver.com>
2024-05-02 16:45:37 +0900

    🔨 : saving엔티티 순서 수정
    Former-commit-id: 08c5e6b63bad684d3e572acce0cc2c7344213de8

--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/saving/SavingOptions.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/saving/SavingOptions.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b20fb5638f487c486d67a4f103460cefc4475c731854581ee30ac4a9cbefc1d
-size 1657
+oid sha256:ae65a5ec491eb81886652bd2ab7b2248ed838ca2e2ac56141ddff4be1cadf550
+size 1667
