9cd822bd6ab2c6c5e137a67a94ec279c0fcd49fc
오승현 <timber3@naver.com>
2024-05-02 11:17:49 +0900

    🐛 : saving 디렉토리 수정
    Former-commit-id: 5ddd1892af29b25ad41705bf14595e3434cff207

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/controller/SavingController.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/controller/SavingController.java
@@ -1,7 +1,3 @@
-package com.ssafy.nhdream.domain.saving.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class SavingController {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:6ba09182bf6aaae289fd27ccb74a474404bd4df6752c8c2d51efc1065555080d
+size 1604
--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/service/SavingServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/service/SavingServiceImpl.java
@@ -1,4 +1,3 @@
-package com.ssafy.nhdream.domain.saving.service;
-
-public class SavingServiceImpl {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:82a71a497f6e5e676f949ac4584e8036b56ac5b7ebd79acfd691c6d094219743
+size 690
