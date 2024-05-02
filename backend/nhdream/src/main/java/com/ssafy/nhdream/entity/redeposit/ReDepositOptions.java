b8403468677daed1d3ef7f33bc4242997551a195
권현준 <bonheur9813@naver.com>
2024-05-02 13:24:40 +0900

    🐛 : 엔티티 오류 수정
    - 연관관계 매핑 @Column에서 @JoinColum으로 수정
    - FK 위치 변경
    
    
    Former-commit-id: 760e2c8cf13d98310f644769dedb2a5810a9ba99

--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/redeposit/ReDepositOptions.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/redeposit/ReDepositOptions.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89143dfc1107939b2d9e397a1cf682c6b529d68b52bb3e3a7a4c0cf180e2fa88
-size 1656
+oid sha256:02da0b2fa55ae368ef7cef64d8b633b80706208a2d89c8b2e224bae718b9e24e
+size 1501
