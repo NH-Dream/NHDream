b68ac2e7bc6b888d6b09fc104ea8f47d79d2e80e
오승현 <timber3@naver.com>
2024-05-02 14:44:00 +0900

    ✨ : VoucherItemTransaction builder 추가 및 주석 추가
    Former-commit-id: fd7b6432f83dd537196cda01f805f94f9510ea52

--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/voucher/VoucherAffiliate.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/voucher/VoucherAffiliate.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72d54d772b1d6b65b73202abf961c3f8667b414deda72e8d706f631cc94c0c55
-size 1256
+oid sha256:59e916f62b3b8a2efa3e8a3d105e72cc20a2acc2d2cb0df8a76ad8bf9db243a7
+size 1200
--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/voucher/VoucherItem.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/voucher/VoucherItem.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7407ff9d6c042a1f5bed34b2724f6e383ef9595ab02c892581add5e3228c079
-size 1189
+oid sha256:5f9de9c3998323b797bb38a1dbb83d12dcf5132d088d1d9bdd11c183c9e2d19a
+size 1479
