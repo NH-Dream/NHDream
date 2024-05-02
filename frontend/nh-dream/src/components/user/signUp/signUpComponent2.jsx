ad247714bd44669a3ac0e5cf4e0c90cab4a0e323
이정민 <bona3377@naver.com>
2024-05-02 09:51:57 +0900

    💄 : User Login, SignUp page 생성
    Related to : S10P31S209-129 #done
    
    
    Former-commit-id: d679f890231dbab5041ea4636e14094a9a8c0510

---
+++ frontend/nh-dream/src/components/user/signUp/signUpComponent2.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:006ec1c82030c28baf82f5c938faf48e9a4bad65f9b1147beea233de9473e98e
+size 1633
---
+++ frontend/nh-dream/src/components/user/signUp/signUpComponent5Student.jsx
@@ -0,0 +1,7 @@
+export default function SignUpComponent5Student() {
+  return(
+    <div>
+      <p>여기는 signUp5Student</p>
+    </div>
+  )
+}
