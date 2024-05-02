4c56d52e040e6e9c55fe6bc1521e4ddb8f42a94f
김준엽 <1025jy@naver.com>
2024-05-02 13:22:54 +0900

    🔨: Loan Entity 수정
    Former-commit-id: 382fdf564991c18c07347fccc4d4557960df3296

--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanTransaction.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanTransaction.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50765c1413a02cb1c020a4151a526832ae800a67d3adb36ae8cb430d8a739c07
-size 1323
+oid sha256:46cb9c45b9b3e0300fd3ecf8e4931b4e3300c1e72a24b160e6e144f080d0da4a
+size 1327
