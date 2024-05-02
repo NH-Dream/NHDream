6038026b4b69a08d122989859cf22420e6dcbd82
조현제 <abb6575@naver.com>
2024-05-02 13:40:29 +0900

    🔨 : SavingRepository코드 수정
    Former-commit-id: 0a7c03417c948d912cde4a6a103120109c2aefd9

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/dto/SavingProductListDto.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/dto/SavingProductListDto.java
@@ -1,15 +1,3 @@
-package com.ssafy.nhdream.domain.saving.dto;
-
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-public class SavingProductListDto {
-
-    private int id;
-    private String name;
-    private double maxInterestRate;
-    private int maxMonthlyLimit;
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e7f6d7fecf74b77bdd6becd1cecb8d072e07175c1a30269d1519f92d672a3d4a
+size 609
