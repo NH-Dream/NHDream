9468db05c44d33da405e1abc262f72ad9e49e66f
조현제 <abb6575@naver.com>
2024-05-03 15:23:13 +0900

    ✨ : 적금 상품 등록api 추가
    Former-commit-id: 83f6179696ae41b28b29deb88b3e188d9b737619

---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/dto/SavingOptionCreationReqDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b0933d0134d9aaf1be2a3eb5cb2bd5ab06af2f2b3b449981124df3b8dc59f8f6
+size 872
--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/repository/SavingProductRepository.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/repository/SavingProductRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f2372446ef7fa6725bea1729f1bbaf8188bea5585e90b6082ef8c3751cbc9b6
-size 849
+oid sha256:1b81813898766ce658f54922034fb5cb85e7792abfb748e8e9c7e00ba79dad56
+size 857
