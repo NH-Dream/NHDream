f17acc2b851d3c5ba0f5475d071a08232fe75ec3
권현준 <bonheur9813@naver.com>
2024-05-03 15:59:05 +0900

    ✨ : 아이디 중복 검사
    - 아이디 중복 검사
      성공시 200 코드 및 "Success" 반환
      실패시 400 코드 및 U-002 예외처리
    
    Related to : S10P31S209-128 #done
    
    
    Former-commit-id: feb01ba62583a726712b560e69f9610e37fb3a0a

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/user/service/UserService.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/user/service/UserService.java
@@ -5,4 +5,7 @@ import com.ssafy.nhdream.domain.user.dto.JoinReqDto;
 public interface UserService {
     // 회원가입
     public int join(JoinReqDto joinReqDto);
+
+    // 아이디 중복 검사
+    void checkIdDuplicate(String id);
 }
