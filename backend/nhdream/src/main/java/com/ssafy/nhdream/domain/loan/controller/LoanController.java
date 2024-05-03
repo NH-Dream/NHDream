e824458738466de9fded22bc73da0c29c2774e3a
김준엽 <wnsduq1025@gmail.com>
2024-05-03 16:03:00 +0900

    ✨: getLoanList 추가
    Former-commit-id: 8dc9418227ed3ce3246ffa0958bdc55b80f59ba6

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/controller/LoanController.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/controller/LoanController.java
@@ -1,7 +1,3 @@
-package com.ssafy.nhdream.domain.loan.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class LoanController {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b094360bbf9954158f68cda5ece86934d363e6ee12f2e98b87717a363253f497
+size 1540
