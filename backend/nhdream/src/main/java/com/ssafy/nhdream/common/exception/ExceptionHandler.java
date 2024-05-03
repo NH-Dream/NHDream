58359e2102a62ce3da7404b6310b31f3538f7014
조현제 <abb6575@naver.com>
2024-05-03 15:39:34 +0900

    ✨ : 형 타입 에러핸들러  기능 추가
    Former-commit-id: 6f2092eed57f422cd29fdee4abd4031e147619cf

--- backend/nhdream/src/main/java/com/ssafy/nhdream/common/exception/ExceptionHandler.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/common/exception/ExceptionHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89b55c8837eafaa34a1bd150210bb3bdf15f3a2b01f3816b260d0e68160ddcde
-size 1817
+oid sha256:edba229f3f729a8a0f4d3390b3d349340f6d906eaca5b78cb053f2270afd3158
+size 2553
