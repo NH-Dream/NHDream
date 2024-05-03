b378f8ce524f8642a523e8413804f8d04fc52046
이정민 <bona3377@naver.com>
2024-05-03 10:47:55 +0900

    ✨: p하단바 유무 페이지 수정
    Former-commit-id: 03fe68367b0d3f368b0442e47f73c47f83fc1124

--- frontend/nh-dream/src/App.jsx
+++ frontend/nh-dream/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c35fdf249053abcf142d743fdb6b45ab5721be35cdf5fc97fc11a16c4e5c1157
-size 2063
+oid sha256:5cdde03d511cea5797ac19555364c7343d88811a50356b409e4aff8d210f324d
+size 2190
