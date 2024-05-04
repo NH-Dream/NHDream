f629d6d5c8b312ca5ba79713d1195a728b67b25a
권현준 <bonheur9813@naver.com>
2024-05-04 19:26:04 +0900

    ✨ : 귀농 인증 신청 기능 구현
    - 귀농 인증 신청 기능
      성공시 200 코드 및 "Success" 반환
      신청자 아이디에 해당하는 유저가 없을시 400 코드 및 U-001 예외처리
      중복된 사업자 번호로 인증시 400 코드 및 U-007 예외처리
    
    Related to : S10P31S209-130 #done
    
    
    Former-commit-id: 1362bee8966424726f3fa9e36dd7084da2ee7ee1

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/user/service/UserServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/user/service/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79f034ecd9e796aaef2ac8ed436961b4ff2cda45dcc2f52cddbd04a5da2dae8a
-size 2935
+oid sha256:7668bdfa6540cd37f6d9906d6bc6666fd2a66756f85e049896318778dc286b93
+size 4211
