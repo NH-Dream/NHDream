8b7da18699e0015bdaff71f57071604db2540bf7
김준엽 <wnsduq1025@gmail.com>
2024-05-05 17:35:52 +0900

    🔨: LoanAccount Entity 수정
    Former-commit-id: 8a686dcd0e1f53324fbae0195c825d5de0171b0d

--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanProduct.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanProduct.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74c962fd7ea70461579cc40550f5b122ee5ea3d822f2bb82285c4f94d21e47e4
-size 1119
+oid sha256:516e961176d7667257ecd302cd1c2eb61ab25ff512ec239213f1489b9a63476a
+size 1653
