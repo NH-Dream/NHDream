ca6c63e6ae232c8b21f6b92c9223d8bd85086fb3
김준엽 <wnsduq1025@gmail.com>
2024-05-05 18:00:44 +0900

    🔨: getLoanList 수정
    Former-commit-id: 85629c189f2061846807d0febcaf668b8244d450

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/service/LoanServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/service/LoanServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6576bbb089d262d0a0578887ca75afb826fdf45c07b6a1b596ddbade0b2b8d91
-size 1573
+oid sha256:4e18910fc4a4d1c437f5a68a6b385992e2ae5337b77bf3bc48d6e7524cb31dc6
+size 1598
