970415ce6b4fa04ab1f0b56400493b7f8869a7a5
권현준 <bonheur9813@naver.com>
2024-05-06 23:58:03 +0900

    ✨ : 이메일 코드 인증 API 연동
    - 이메일 코드 인증 API 연동
    - sweetAlert 인증실패 모달 설정
    
    Related to : S10P31S209-153 #done
    
    
    Former-commit-id: 85bf2c04f63afe042b001dcb48631b40cc6b12cd

--- frontend/nh-dream/src/components/user/signUp/signUpComponent1.jsx
+++ frontend/nh-dream/src/components/user/signUp/signUpComponent1.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:918416f1a6230fa13a0035eeaa843cc13d2e24d806d769cc5c441924caa734cc
-size 5457
+oid sha256:533309668cf4f4877ad4113fcf361716bb7147526e35f6b286a6ed491ec26744
+size 6494
