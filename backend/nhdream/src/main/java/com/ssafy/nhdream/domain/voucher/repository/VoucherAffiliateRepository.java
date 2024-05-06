7bd7b99bf566e53da0d4990dcfb91122f56e46b9
“오승현” <timber3@naver.com>
2024-05-07 00:05:24 +0900

    ✨ : affiliate 수정, 삭제, 리스트 불러오기
    Former-commit-id: 0e807f6ce59cb8a0b0f0d4511ff36a361605fe36

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/repository/VoucherAffiliateRepository.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/repository/VoucherAffiliateRepository.java
@@ -1,8 +1,3 @@
-package com.ssafy.nhdream.domain.voucher.repository;
-
-import com.ssafy.nhdream.entity.voucher.VoucherAffiliate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VoucherAffiliateRepository extends JpaRepository<VoucherAffiliate, Integer> {
-    
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ba76b61bb233d48a598343a3085531a5e095c650df9cb2f9ccdd76e36481c95
+size 723
