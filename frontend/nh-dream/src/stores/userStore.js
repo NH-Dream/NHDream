3f8ee85f7ad1f520aa15d16393434f5c5f97bfad
권현준 <bonheur9813@naver.com>
2024-05-07 04:49:25 +0900

    📝 : 성공 & 실패 알림 모달 분리
    Former-commit-id: fcfa9d1912e04a5e19c9c566ea90bfa1cff6e946

--- frontend/nh-dream/src/stores/userStore.js
+++ frontend/nh-dream/src/stores/userStore.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4342fdc326df57657af85ad6bb02389a05c00282da71e1d1cfa08bcc76a0def
-size 2456
+oid sha256:62e6132dfd5e853dc85ca0dec7ce302daaa5235bf240b03415fb92790b4e6c1d
+size 2900
