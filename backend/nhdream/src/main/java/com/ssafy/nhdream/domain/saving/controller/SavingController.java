4cf93af45f12a5fd7ad7c1c3e84db2f9653e2b4a
조현제 <abb6575@naver.com>
2024-05-06 22:31:38 +0900

    ✨ : 새적금계좌 거래내역 조회 추가
    Former-commit-id: 4f532256fb9dc011fb0163f58d1a44f756ad8ad4

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/controller/SavingController.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/controller/SavingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf73df8b4e58848b7ba7e4e97694e2e391b95371ed5a16de0fa03259438c1b81
-size 4065
+oid sha256:74d84b36bc095e2b0a241406635785d8f08fbe9a2e8e67a2a1f443f844766a63
+size 5048
--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/service/SavingService.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/service/SavingService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb2878388545b044ed5eaf5e4c2aa6bb01be91ee1cd8a0e155e997e4b0c95ab5
-size 818
+oid sha256:1ada921fbd2feb4319051283bd9c2aa7d5c551feeaff34cf29c56726fd32af83
+size 731
