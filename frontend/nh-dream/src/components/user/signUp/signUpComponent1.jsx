6b35461bd273796ba1645b9e820b6ad0b76a987f
권현준 <bonheur9813@naver.com>
2024-05-06 23:32:55 +0900

    ✨ : 이메일 인증 코드 API 연동
    - 이메일 인증 코드 API 연동
    
    Related to : S10P31S209-152 #done
    
    
    Former-commit-id: 846d91fd7c5f78f34e24fb2875c34c4158ecb53a

--- frontend/nh-dream/src/components/user/signUp/signUpComponent1.jsx
+++ frontend/nh-dream/src/components/user/signUp/signUpComponent1.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdc7bda95fe0ad2c2be171cff87300292d4f4c8320024bd7585cf29a68c0f72d
-size 4273
+oid sha256:918416f1a6230fa13a0035eeaa843cc13d2e24d806d769cc5c441924caa734cc
+size 5457
