8f664b7186fda6c07b6f5731bfa98045ae89389b
권현준 <bonheur9813@naver.com>
2024-05-07 13:20:03 +0900

    ✨ : 로그인 API 연동
    - 로그인 API 연동
      Access 토큰 LocalStorage에 저장 및 zustand 유저 상태 저장
    
    Related to : S10P31S209-159 #done
    
    
    Former-commit-id: b1a6aa233d9791053f09191ec69ca6eef68e4796

--- frontend/nh-dream/src/components/user/login/loginComponent.jsx
+++ frontend/nh-dream/src/components/user/login/loginComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d33ea76c77fae085f26441418787e13434722ff563e9b220f4e9577b6ad08587
-size 1932
+oid sha256:ca659d091aca4cafbe1425c51ee96cd151dcda698b76cf752f62a4f6b20daacc
+size 3433
