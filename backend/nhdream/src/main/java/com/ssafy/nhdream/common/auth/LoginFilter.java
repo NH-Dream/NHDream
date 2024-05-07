d8a884885b45a6d56c1059bf8a443c0b7f131720
권현준 <bonheur9813@naver.com>
2024-05-07 12:40:19 +0900

    🔨 : Token 관련 클레임 변경
    - UserType 의 타입 변경(UserType -> String)
    
    
    Former-commit-id: 88afa6161d0cd2713b06163242365f7b97f3a4c9

--- backend/nhdream/src/main/java/com/ssafy/nhdream/common/auth/LoginFilter.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/common/auth/LoginFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01dfc129a7bddc69b0996438f34ae4846d3c1cc2de5f396eb1f7b1f747d2f1ba
-size 4872
+oid sha256:b3f19c6713fff300ea34d58d0b24754ca367efdda89fd44030e2754ba3ef7e18
+size 5017
