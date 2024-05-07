b80f7dae864e252269d52b0853e74fab397a6049
이정민 <bona3377@naver.com>
2024-05-07 20:14:09 +0900

    🔨 : router 코드 수정
    Former-commit-id: c32a55555e57e2a6f0ccaaed5575292a52813732

--- frontend/nh-dream/src/router.jsx
+++ frontend/nh-dream/src/router.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec1d541ced98ad2ede7fcfc52ab153bf25272bc2591cc9363ffc5643772197dd
-size 5814
+oid sha256:16aa62a409993d05aeb1e346fa296914da1e0ab076b98cf2777ca6a7ad01bdeb
+size 5812
