d71a980a0bc3f4aa4f8f4722d301c8ca5a334016
김준엽 <1025jy@naver.com>
2024-05-07 20:30:33 +0900

    ✨: deleteLoanProduct 추가
    Former-commit-id: ca8c083daded8e4e4d8b0e381e30186fe78253e2

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/repository/LoanProductRepository.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/repository/LoanProductRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:567ed67af3c99a6e0ef5f3f0d5ad8f5edb0889527547fc3419e351e202ad1dd2
-size 930
+oid sha256:1c618dda01e989702307528a191e97d0d306181c88e89b4f24f03759f861cec5
+size 1135
