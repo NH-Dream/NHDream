6e265abb5a9530e5da89fb62d684d8cec39586c8
권현준 <bonheur9813@naver.com>
2024-05-07 12:31:14 +0900

    ✨ : 로그아웃 기능 API 연동
    - 로그아웃 기능 API 연동
      localStorage access 토큰 삭제
      Zustatnd 회원 상태관리 초기화
    
    Related to : S10P31S209-160 #done
    
    
    Former-commit-id: 19c96238ca67137920bb3580d509fc87b0b54819

--- frontend/nh-dream/src/components/user/login/judgingComponent.jsx
+++ frontend/nh-dream/src/components/user/login/judgingComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fe03aaf1f375e8292bcb59c3a9910264e3a2f05cf32d31c53c5240e06f0cd9b
-size 1066
+oid sha256:c0a9b4fe352689671ac6267b313c7032302f65a8b09be0a02851641e178ee06d
+size 1433
--- frontend/nh-dream/src/pages/mypage.jsx
+++ frontend/nh-dream/src/pages/mypage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b9241c839d550f50cf255a807d947b86c198b8b22d0b3be00e97553dfe9a9c6
-size 1863
+oid sha256:45f8f27a16a73202fe531b6355dc6cfb9a0afc2ca6f98736ae05dbde665c7dca
+size 2329
