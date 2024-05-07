99e23fab04b5e363d7d5d218e0147493be66bb53
조현제 <abb6575@naver.com>
2024-05-07 20:42:44 +0900

    ✨ : : 적금 가입에 블록체인 로직 추가
    Former-commit-id: 814e531df78fc7254bdf2708f66cd4d70cb5a4b6

--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/saving/SavingTransaction.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/saving/SavingTransaction.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:372a67b4a3a96132199be3e62107f1a4f17dd5bcaf1da8b5b40915ad8c2e6b3c
-size 1988
+oid sha256:30a4a00d546e2e952b840398f70ff7c1c5cb91b46943f9c0cd8e3eb53cf2c884
+size 2043
