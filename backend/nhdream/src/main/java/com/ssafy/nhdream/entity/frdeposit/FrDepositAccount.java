b2a9ae76b39527d438c98de5f4e8a78f492251e2
조현제 <abb6575@naver.com>
2024-05-07 13:32:56 +0900

    ✨ : 유저에 지갑 추가 기능 추가
    Former-commit-id: 3c482f3970fcbf3ec4e4a661da95f7492a7423eb

---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/frdeposit/FrDepositAccount.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3d55d7c1b30f73efe2cecca606c5ed6357bc20ab5be43376dada82d8c2076ed7
+size 1595
---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/frdeposit/FrDepositTransaction.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2d2d915024db69cb54b7a598e672466ba96ab6bf37f932d1cfc8834a17c67de7
+size 1915
