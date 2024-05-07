22ccc8e8f693c79baa2ad2a979fecaade575d760
이정민 <bona3377@naver.com>
2024-05-07 19:58:09 +0900

    🔨 : 예적금 코드 수정
    Former-commit-id: 09087466495c05b2c8a2eb557781df5fd69d7cad

---
+++ frontend/nh-dream/src/pages/loan/loan.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e6008414adccc36d28fb05c1702371611842fdf6729bf536badd2357beb43def
+size 1130
---
+++ frontend/nh-dream/src/pages/saving/realSaving/savingDetail.jsx
@@ -0,0 +1,9 @@
+import TopBar from "../../../components/common/topBar"
+
+export default function SavingJoinDetailPage() {
+  return (
+    <div>
+      <TopBar title="적금" />
+    </div>
+  )
+}
