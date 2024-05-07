88ca6f6b189ca16c433f8cbdb1d15d7b6f8e1250
권현준 <bonheur9813@naver.com>
2024-05-07 12:53:05 +0900

    🔨 : 토큰 claim  수정
    - 토큰 클레임 수정 : 지갑 주소 추가
    
    
    Former-commit-id: 730ddfa6e703f8c1a21f1708fe3c0e415668b925

--- backend/nhdream/src/main/java/com/ssafy/nhdream/common/auth/LoginFilter.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/common/auth/LoginFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3f19c6713fff300ea34d58d0b24754ca367efdda89fd44030e2754ba3ef7e18
-size 5017
+oid sha256:2b31a24e3d950685bd785f43d009fd9f32aa5f20a50c283e84e25dd9fda1b2e8
+size 4828
