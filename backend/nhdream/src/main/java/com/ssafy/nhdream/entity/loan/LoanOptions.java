10eb175d4043273747008eebea4991857055da13
김준엽 <1025jy@naver.com>
2024-05-08 22:38:28 +0900

    🔨: Entity 수정
    Former-commit-id: ecd232c0b303aab1f1ce512a108cb150181703a9

--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanOptions.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanOptions.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4d2940c974b9d1706c5a910ed450c549f1a39a76ddc078d1791bc41543ca52b
-size 2527
+oid sha256:8279b649ee9803f514461b25cf66584684404c2eeaf320be5866b8840cd4db73
+size 2582
