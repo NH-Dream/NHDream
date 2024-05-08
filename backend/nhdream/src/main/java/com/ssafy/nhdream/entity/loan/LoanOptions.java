c554368afb988c376e45b36a5ee1287955612997
김준엽 <wnsduq1025@gmail.com>
2024-05-08 14:29:34 +0900

    ✨: createLoanAccount 추가
    Former-commit-id: a02fb367f4f88e9f074d23046866d9b9bf0aad05

--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanOptions.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanOptions.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ef1c04e9fe6d984ee0fa8468ba48403a2e0680e0e1586329454ab7b861c1b39
-size 2794
+oid sha256:e0cf11ab2e3d506dac54c16f8b8d94658b3c378c95ae7a0e7b7be3894a17a216
+size 2977
