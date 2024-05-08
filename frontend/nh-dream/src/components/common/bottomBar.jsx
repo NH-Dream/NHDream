f4c64de6b707fcb74d43ffb49c233e6cecf84e66
장세정 <wkdtpwjd29@naver.com>
2024-05-08 13:17:54 +0900

    🔨 : 코드 수정
    Former-commit-id: 4c8a167e72012dd9235aafbe4ac5f32f14973472

--- frontend/nh-dream/src/components/common/bottomBar.jsx
+++ frontend/nh-dream/src/components/common/bottomBar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43be8250c67b40820c607d04db4ec4fc1e0f7ac4b40cf8192e9be0490a957738
-size 2198
+oid sha256:c9376ef0e8cbafb34814ba183aa647a3018b14f0a5ddbde06db797394d540899
+size 2201
--- frontend/nh-dream/src/pages/main.jsx
+++ frontend/nh-dream/src/pages/main.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:383e2e6701c31932a9fbf475d659fc9b52c820807106fcc94ab1b89d328b142d
-size 655
+oid sha256:c510638cd5d542ac5e2c567af4c45d6e24a574d629063d9fd511299d99da3f92
+size 707
