77c54d2aa04157b68148492bb3ec7aaa8a66a58d
조현제 <abb6575@naver.com>
2024-05-08 17:52:53 +0900

    🔨 : 자유입출금거래내역 변수명 수정
    Former-commit-id: ba6b86f61c559479a7a4b0de172619852782e3fa

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/frdeposit/controller/FrDepositController.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/frdeposit/controller/FrDepositController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75273be400d2ea9f649c7eb6a40493333bfea7e24452da74c5a241925c03e249
-size 4542
+oid sha256:43771a2506812d611beddebd2319c1bba48897e15a1a990ba6e2a5e83bd614a0
+size 4536
