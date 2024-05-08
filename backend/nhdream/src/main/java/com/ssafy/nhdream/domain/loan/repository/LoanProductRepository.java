51cb3a27d7b76a8e70fb615cdcedd5b078c0954d
김준엽 <1025jy@naver.com>
2024-05-08 23:12:23 +0900

    🚧 : 작업 도중 저장
    Former-commit-id: 2c0b2fcaa3b95661cef89b54f8304a9a6b08f8f8

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/repository/LoanProductRepository.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/repository/LoanProductRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2069c2c46023ba5ec749325414c3e2465ad9b9738f6bacc969c35f4fed9dc1c9
-size 1218
+oid sha256:60daea3a560d5534a16714131c5d2d9bb37474d105893b406507fce280e749c7
+size 1219
