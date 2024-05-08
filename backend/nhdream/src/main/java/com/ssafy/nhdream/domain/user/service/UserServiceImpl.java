93e65aab9536b1052d20fa1de55369303bb70529
권현준 <bonheur9813@naver.com>
2024-05-08 13:54:50 +0900

    🔨 : 사업자 등록 로직 변경
    - 공공데이터 사업자 등록번호 조회 로직 추가
    
    
    Former-commit-id: 9420f60ef6609028a05052d095cadb21f5adc633

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/user/service/UserServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/user/service/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:628af73c5d524892bee51efaec56f5d3052e4cb83a7a03f717be1d705310a392
-size 6744
+oid sha256:84823908a579e6b4be3e80bdc629637ac2fe8b934958162bc6493df3bd4e53f4
+size 7286
