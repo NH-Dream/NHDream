3f19903248e3d52234c9c6065668b075b6820ca1
조현제 <abb6575@naver.com>
2024-05-08 14:33:46 +0900

    ✨ : 회원가입시 지갑 테이블 생성기능 추가
    Former-commit-id: 5c8aa8b8377b8e7eae737b40f74a79842669254a

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/user/service/UserServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/user/service/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84823908a579e6b4be3e80bdc629637ac2fe8b934958162bc6493df3bd4e53f4
-size 7286
+oid sha256:4a9214177afe4857bf93ea4a8f2be35cdb8bae2ee381a10ea85bb342a2c58165
+size 8305
