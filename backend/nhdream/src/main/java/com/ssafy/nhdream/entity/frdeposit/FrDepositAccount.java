a1b13186adc9f2a9b51dd9122db03c0cc9eb8149
조현제 <abb6575@naver.com>
2024-05-08 22:23:10 +0900

    🔨 : 거래후 잔액 코드  수정
    Former-commit-id: dd7f2ba0400790659f33cc04f0fda80b75323d1d

--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/frdeposit/FrDepositAccount.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/frdeposit/FrDepositAccount.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3e0048bb955e1ba81b726712bf776a91ecfe010c7e4ffd688f7dc013f9feb5c
-size 1918
+oid sha256:e14392848452566cb51038854bdfb08ce52267fc17e109331615aaa3b122c384
+size 1923
