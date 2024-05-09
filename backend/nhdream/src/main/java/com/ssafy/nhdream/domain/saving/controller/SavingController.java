89d1b91b75fd8a7cad1ec1377cb434ce863d03d4
조현제 <abb6575@naver.com>
2024-05-09 15:50:03 +0900

    ✨ : 적금 히스토리 페이지용 api 기능 추가
    Former-commit-id: e17940455793870778939dc5890e19a6ed88954a

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/controller/SavingController.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/controller/SavingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45bc3950bf84a0ca11f644fd35e8cc5d7538e6ea3798f52831db47014ce18189
-size 5054
+oid sha256:dc1351d63f795b35cd803dbf1fb9f3787493843ed5820176b6829f95e98d4f44
+size 5008
--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/saving/dto/SavingTransactionListResDto.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:de47ade39729e3ea9fc732ec8dc3ea006dd6e4db0cfdc201b2a09d9d6353eda0
-size 860
