6a1855ecfd78c11031997790d1dbb80644a23366
장세정 <wkdtpwjd19@gmail.com>
2024-05-09 13:02:07 +0900

    ✨ : 지갑잔액,거래내역,이체하기logic
    Former-commit-id: aa25b53c61393a01459f78a769447699c38938fd

--- frontend/nh-dream/src/components/mypage/walletHistory.jsx
+++ frontend/nh-dream/src/components/mypage/walletHistory.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb59945b8ec52f5173a21f589e0e39d7aba54780768eeefabfe645e803625281
-size 1043
+oid sha256:f54f73b9e15d6e37caeac8537761055889a8a1140179a113806905d86ccb4cdf
+size 1195
--- frontend/nh-dream/src/components/transfer/wallet4.jsx
+++ frontend/nh-dream/src/components/transfer/wallet4.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98c21fccfa34bcbfa38e3741c35ea64140a910f57310958577340db002aea871
-size 771
+oid sha256:b546343a169f8d68189718d89ec78c3cd1b2cf192d5d174121b201adfe881a58
+size 875
