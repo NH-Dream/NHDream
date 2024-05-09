00b73d503584ae57a34512052b45497784f361cb
권현준 <bonheur9813@naver.com>
2024-05-09 16:05:30 +0900

    ✨ : 정기예금 상품 리스트 목록 반환
    - 정기예금 상품 리스트 목록 반환
      성공시 200 코드 및 목록리스트 반환(식별ID, 상품명, 최고이자율,
      최대한도)
    
    Related to : S10P31S209-250 #done
    
    
    Former-commit-id: f6ad88f086327f8365f9916061bb96de4be5c983

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/repository/RedepositProductRepository.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/repository/RedepositProductRepository.java
@@ -1,7 +1,3 @@
-package com.ssafy.nhdream.domain.redeposit.repository;
-
-import com.ssafy.nhdream.entity.redeposit.ReDepositProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RedepositProductRepository extends JpaRepository<ReDepositProduct, Integer> {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9bf0419aa67cb1a4bccaab2ff77b4e4850f033f0a39472733883c2d5af8332cd
+size 846
