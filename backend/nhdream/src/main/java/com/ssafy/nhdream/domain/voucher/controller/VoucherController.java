066f91deb787481175729480a2c27fa197c9902c
오승현 <timber3@naver.com>
2024-05-09 17:41:49 +0900

    🔨 : 바우처 Multipart/form-data 관련 수정
    Former-commit-id: 58fd8194b40d17cf60be66442afe79b9f263cac7

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/controller/VoucherController.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/controller/VoucherController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6a597fab412e8f70c36ca9317625e01d2e997e74f27eca0a9754be8ed0fe5ec
-size 6471
+oid sha256:c9f62acd888b31149f0fa6e8fb2235facf64e8c7663b1ee32bf73f8f6a3bce89
+size 6949
