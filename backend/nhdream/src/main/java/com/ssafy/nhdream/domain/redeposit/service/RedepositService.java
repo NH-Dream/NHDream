4811098d08a035f920d4517f2d6be2c8b0b46e9d
권현준 <bonheur9813@naver.com>
2024-05-09 14:16:15 +0900

    ✨ : 정기예금 상품 생성 기능 구현
    - 정기예금 상품 생성 기능 구현
      생성시 201 코드 및 생성엔티티 PK 값 반환
    
    Related to : S10P31S209-256 #done
    
    
    Former-commit-id: b6fef791d3da0ced2d8f3a1e50660c352ecfadbe

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/service/RedepositService.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/service/RedepositService.java
@@ -1,4 +1,9 @@
 package com.ssafy.nhdream.domain.redeposit.service;
 
+import com.ssafy.nhdream.domain.redeposit.dto.RedepositProductCreationReqDto;
+
 public interface RedepositService {
+
+    // 정기예금 상품 생성
+    int createRedepositProduct(RedepositProductCreationReqDto redepositProductCreationReqDto);
 }
