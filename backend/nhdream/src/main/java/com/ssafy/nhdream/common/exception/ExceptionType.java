0dae85144108c5f7b4e60ecb22068da52e910efe
조현제 <abb6575@naver.com>
2024-05-10 10:43:44 +0900

    ✨ : 지갑 비밀번호 변경  기능 추가
    Former-commit-id: 25a33f3bf1fb4eefc736f4a06078bf849ad86fa4

--- backend/nhdream/src/main/java/com/ssafy/nhdream/common/exception/ExceptionType.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/common/exception/ExceptionType.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97aaa521314aa134d17b867a785974054e7d366583670b6b487d6b5e5a7ee211
-size 4883
+oid sha256:de4b1b671d03adce884e70a754a89f5a9f58bebb858e54be372774b973c3b89f
+size 5040
