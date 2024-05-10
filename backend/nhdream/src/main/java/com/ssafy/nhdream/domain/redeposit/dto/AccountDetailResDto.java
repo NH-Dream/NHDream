120e3cebe54822e2e9566dd1e9555f7952365c16
권현준 <bonheur9813@naver.com>
2024-05-10 13:43:08 +0900

    ✨ : 정기예금 가입계좌 상세정보 확인
    - 정기예금 가입계좌 상세정보 확인
      해당 유저가 없을 시 U-001 반환
      해당 예금 계좌 없을 시 R-003 반환
    
    Related to : S10P31S209-224 #done
    
    
    Former-commit-id: 3598f897f833b06208ac4bf5a3644afe704f4406

---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/dto/AccountDetailResDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1b8b01ba8a5c0874ef0b084febde62bea5d206d80fc423309eafea4a56e9a748
+size 890
--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/service/RedepositServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/service/RedepositServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a0e4045dd6085249e25198dafca3f7f11d7f48e11923d946c98ef7ec879b78a
-size 8226
+oid sha256:20b21023db9592577d755d0ff7618193b4b8c1218f385da70deaeb89b0a47c15
+size 9241
