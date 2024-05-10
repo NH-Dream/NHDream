1dcb35b9b0e86e2b876bf46de06cdf801a61409d
오승현 <timber3@naver.com>
2024-05-10 14:01:25 +0900

    🔨 : 상품 조회시 바우처 명 전달
    Former-commit-id: d07b6907e20510e09a6eb097c33eb24debd0a88f

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/dto/VoucherItemResDto.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/dto/VoucherItemResDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73d4b4ecd805ebf1dd16b78e229dd824853860b77503038a654518e1fdac6ea2
-size 756
+oid sha256:19f8ed5d55ae2e0d00e6027d002be9e5a40d7f013eafed35809676c09e31bcea
+size 856
