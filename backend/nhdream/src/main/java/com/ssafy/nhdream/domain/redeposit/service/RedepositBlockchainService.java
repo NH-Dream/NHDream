1353095d71a61647611df697fe77a4052fbda303
권현준 <bonheur9813@naver.com>
2024-05-10 23:08:38 +0900

    🔨 : 이자율 소수점 정책 변경
    - 반올림에서 버림으로 변경
    
    
    Former-commit-id: 6a7829358f2c57e59f3b3dffd201e0a623fa0dec

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/service/RedepositBlockchainService.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/service/RedepositBlockchainService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c494f704c095d69c2eb62b66df097462a15961c7a70f5a1a64405d03fd7d1c0
-size 4097
+oid sha256:a38e1e1af6791576e3d4edc3ce8ba4a0cdb9c94d02a1a8694d6196e17bcead6b
+size 4094
