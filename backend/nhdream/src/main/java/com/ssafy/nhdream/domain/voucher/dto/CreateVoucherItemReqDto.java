1a3218ec7127028237262629228e489f9f9c0dfa
오승현 <timber3@naver.com>
2024-05-10 13:42:19 +0900

    🔨 : affiliate 지갑 추가
    Former-commit-id: c2072b43ead8e861686717c273b71258a26af426

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/dto/CreateVoucherItemReqDto.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/dto/CreateVoucherItemReqDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:634e96fcf4e8aa14da39886d4e3b31a75045f7f571c0e244fa9e278290fe0833
-size 937
+oid sha256:eeb8e018981c8b3fe2cf28f59f9c95f8ecd21d9ef137452df43ab7fa5bb7f7f1
+size 1105
