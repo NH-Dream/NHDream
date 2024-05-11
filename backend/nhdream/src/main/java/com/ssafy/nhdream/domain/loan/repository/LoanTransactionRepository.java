749f4cb3be6a6be9a54e9ebfde06317bff1537a7
김준엽 <wnsduq1025@gmail.com>
2024-05-11 17:08:44 +0900

    🚧 : 작업 도중 저장
    Former-commit-id: a2eb5bb01953f95aadc379ba3d7edc50cee9528e

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/repository/LoanTransactionRepository.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/repository/LoanTransactionRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:248af92bbc65ad32ffa00d87fb81c7a501bc7d9ca7b9fb5cf181116cf0115766
-size 659
+oid sha256:9c8fdbe4cd6dd51b29da8133d263007b32b60a81e84f5f13995b04b3e86c6518
+size 640
