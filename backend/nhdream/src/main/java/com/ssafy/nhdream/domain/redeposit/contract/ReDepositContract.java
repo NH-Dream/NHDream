cff831acebe7007cdc9e0a10289366728d773533
권현준 <bonheur9813@naver.com>
2024-05-11 16:39:30 +0900

    🔨 : 솔리디티 수정에 따른 Contract 메서드 추가
    - deposit 함수 적용
    
    
    Former-commit-id: 4dbd1ffb82f6f8355736b71fb91c7696e20ac885

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/contract/ReDepositContract.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/contract/ReDepositContract.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d54af2db345d89844b4eaaedb5b06b176487bafcac3e2d0431a62fb3b1cc384
-size 22282
+oid sha256:90a6dec1fee78f8621e3037b963a939d71c5f7fd79fdf8976fe4fe59f2a15113
+size 24122
