3f23e03455c2cdfc2cc054c2ff78921cd9fe0f34
timber3 <timber3@naver.com>
2024-05-11 18:58:34 +0900

    🚧 : voucher item 구매 작성 중
    Former-commit-id: 6d661161519c5398b8e9f1902ee7916e5598793e

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/controller/VoucherController.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/controller/VoucherController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de83f702e4723b5fb2f93c8d7794995d00d858cc520f72ec41a6f703b4533907
-size 7949
+oid sha256:706a6d54355ba24986c43a049e2f2966370e69b601a2f9c5ae6c7e32a7d27cd5
+size 8044
---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/dto/VoucherTokenResDto.java
@@ -0,0 +1,12 @@
+package com.ssafy.nhdream.domain.voucher.dto;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Slf4j
+public class VoucherTokenResDto {
+
+    private int amount;
+
+}
