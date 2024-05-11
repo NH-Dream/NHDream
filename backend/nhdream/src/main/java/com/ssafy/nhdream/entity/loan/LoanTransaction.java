88fd66383c234caf0750ba3cbd68b4a090a89462
김준엽 <1025jy@naver.com>
2024-05-11 17:14:28 +0900

    🚧 : 작업 도중 저장
    Former-commit-id: 7550825efece3ec0bcc4cf8fd3913d446dee8060

--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanTransaction.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/loan/LoanTransaction.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6f7b7f32856ae025087eaa030243d94fb1c409966137f7f9fdc80a399cc96d6
-size 1582
+oid sha256:655f1eb92d52a06b4b62094f44932d475c33ef19bd2339ebb4bb4c8a8e3ad109
+size 1740
