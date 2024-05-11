bd0ad2e3d5582318493bf639821ba5470b610245
권현준 <bonheur9813@naver.com>
2024-05-11 17:34:43 +0900

    🔨 : 정기예금 생성 완료
    - 블록체인 연결 완료
    
    
    Former-commit-id: 321ee1b3cd6c5b012ec146d1da65a20d2e56945d

--- backend/nhdream/src/main/java/com/ssafy/nhdream/common/utils/TransferService.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/common/utils/TransferService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b235ad5aaf3435b12141c88cdead20a87621fa741becae7206f990a22828b9b
-size 8257
+oid sha256:a71d4a4d04665fd9f84e38a2f88530e57e9712dc634e39c12e33dd39b70aeb18
+size 8258
