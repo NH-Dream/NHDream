43727a70a4bef252af7bea58642c663f34f79784
김준엽 <1025jy@naver.com>
2024-05-11 16:09:29 +0900

    ✨: getMyTransaction 추가
    Former-commit-id: 8e5db545c0cf40c9236f1e0b712b2c3229bc4a4d

---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/repository/LoanTransactionRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:248af92bbc65ad32ffa00d87fb81c7a501bc7d9ca7b9fb5cf181116cf0115766
+size 659
--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/service/LoanServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/service/LoanServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f3f2f85d8b1bf9416ebe2a5fbfb57ac56c61eab89712041381cd3cf4f453ae0
-size 17083
+oid sha256:f1f036ae9b3b7e8879f16d068e7cd3217278f01ef948f085b41317b87be9c4c4
+size 17084
