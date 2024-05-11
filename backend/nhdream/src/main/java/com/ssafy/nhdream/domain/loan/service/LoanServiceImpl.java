b7ef6146f5b46b5ca1da543445b9668ad184e121
김준엽 <wnsduq1025@gmail.com>
2024-05-11 19:09:10 +0900

    🔨: transaction 수정
    Former-commit-id: 6a33ceff3c65aea083ef1af34ccc7d980205d242

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/service/LoanServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/loan/service/LoanServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19dc5a35c934e92bd69a0c1c0100b7214feb7717105d44b5048ff9f64a53be8c
+oid sha256:b922e6b457c5d7526c0c91357badba37fee7918a0d75dda27c82456468807a85
 size 20851
