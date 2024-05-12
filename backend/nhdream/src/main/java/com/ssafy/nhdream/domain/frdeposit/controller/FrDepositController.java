3356714b9ba9b3e21c616d78050eff73f513bb65
조현제 <abb6575@naver.com>
2024-05-12 13:21:59 +0900

    ✨ : : 적금 만기, 자동이체 변경 기능 추가
    Former-commit-id: ed9621b1b9de770e5faa666b24ca1a5c447240f6

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/frdeposit/controller/FrDepositController.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/frdeposit/controller/FrDepositController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79beea0f27fd2e11f151131d1ea010fc402b5a7b73a908ccd7a32e98d3f47fbe
-size 8027
+oid sha256:8330c3a610e0024e6533e024603173c723d1832c1cf2ac8541bacf421cbeeecf
+size 8877
