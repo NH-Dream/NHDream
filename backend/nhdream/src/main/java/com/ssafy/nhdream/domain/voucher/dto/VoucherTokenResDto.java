65ddca61d255eeb81bb060675489916ed7261727
“오승현” <timber3@naver.com>
2024-05-13 00:27:09 +0900

    ✨ : DRDC Service
    Former-commit-id: 92ca5e6954de845af26320182a6b1318382c446d

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/dto/VoucherTokenResDto.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/dto/VoucherTokenResDto.java
@@ -3,10 +3,12 @@ package com.ssafy.nhdream.domain.voucher.dto;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import java.math.BigDecimal;
+
 @Data
 @Slf4j
 public class VoucherTokenResDto {
 
-    private int amount;
+    private BigDecimal amount;
 
 }
--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/service/VoucherServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/voucher/service/VoucherServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:288b29148eccbf8048464528ca5a5255027bd5083bfa7060cc5294b2ef3206ca
-size 6863
+oid sha256:716f97d4a931a07231803ff6d1a3696ea85788a52ef5bc83b295b47fc2d1c684
+size 7217
