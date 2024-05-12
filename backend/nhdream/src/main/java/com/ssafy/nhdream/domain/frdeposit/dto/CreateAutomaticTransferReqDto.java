5f71144574c3b7471a422f5dd3ce0fdbebad1437
조현제 <abb6575@naver.com>
2024-05-12 21:04:10 +0900

    ✨ : 새자동이체 ReqDto 유효성 검사 기능 추가
    Former-commit-id: 93312f5303a3f47d36eb167b4c492218d111b8fe

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/frdeposit/dto/CreateAutomaticTransferReqDto.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/frdeposit/dto/CreateAutomaticTransferReqDto.java
@@ -1,27 +1,3 @@
-package com.ssafy.nhdream.domain.frdeposit.dto;
-
-import lombok.Getter;
-
-import java.math.BigDecimal;
-
-@Getter
-public class CreateAutomaticTransferReqDto {
-
-    //보낼주소
-    private String recipientWalletAddress;
-
-    //이체금액
-    private BigDecimal recurringAmount;
-
-    //입금주소
-    private String senderWalletAddress;
-
-    //이체기간
-    private int term;
-
-    //매달지정일
-    private int transferDay;
-
-    //지갑비번
-    private String walletPassword;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3210ce0b0eccbdada52f116f05a47bc55c7cb8b632e707f4be14aa6ee1ba5f7a
+size 1671
