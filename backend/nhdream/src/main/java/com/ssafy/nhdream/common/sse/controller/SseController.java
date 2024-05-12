e68463c40dee6663efe2f065fa18bdf5df6904a1
김준엽 <1025jy@naver.com>
2024-05-12 21:43:16 +0900

    🚧 : 작업 도중 저장
    Former-commit-id: 6a6b1b684d9149a364e9cc4996e58e6c1cf06596

--- backend/nhdream/src/main/java/com/ssafy/nhdream/common/sse/controller/SseController.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/common/sse/controller/SseController.java
@@ -1,9 +1,3 @@
-package com.ssafy.nhdream.common.sse.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/sse")
-public class SseController {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ca8d23a093d84c8338755a276df4949283b41072593634f8eea8c32cb558d866
+size 1102
