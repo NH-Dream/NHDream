20596f130f45a8b1be6db74ad433ad75c3f04316
김준엽 <1025jy@naver.com>
2024-05-12 23:12:30 +0900

    🚧 : 작업 도중 저장
    Former-commit-id: da6e05aa03eed11662d6c3b05086f4a36a7c5caa

--- backend/nhdream/src/main/java/com/ssafy/nhdream/common/sse/service/SseServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/common/sse/service/SseServiceImpl.java
@@ -1,7 +1,19 @@
 package com.ssafy.nhdream.common.sse.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Service
 public class SseServiceImpl implements SseService {
+
+    @Override
+    public SseEmitter subscribe(String userId, String lastEventId) {
+        return null;
+    }
+
+    @Override
+    public void sendTest(String userId) {
+
+    }
+
 }
