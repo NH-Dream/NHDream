bdfecdc63b950c67744d36d60bcb736c0d46a0e3
권현준 <bonheur9813@naver.com>
2024-05-13 02:48:40 +0900

    💄 : NHDC 유동량 조회 UI 구현
    Related to : S10P31S209-234 #done
    
    
    Former-commit-id: 6abee4eeefbc01cd13ec1f11ad9f6b16d52d8317

--- admin/nhdream/src/components/monitor/detail/NHDC.jsx
+++ admin/nhdream/src/components/monitor/detail/NHDC.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:180495948a151b825c1fba37d420ca71f1d655b136b59070277252d1dafe9f19
-size 805
+oid sha256:db1987843116a9e7ef5eee51095187329c5f8caadf717a894b84627865722dbd
+size 2144
