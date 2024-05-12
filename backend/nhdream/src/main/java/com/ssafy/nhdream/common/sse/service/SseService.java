3cef343e31835b6738dc8b6c2952d3587b2f0870
김준엽 <wnsduq1025@gmail.com>
2024-05-12 23:12:30 +0900

    🚧 : 작업 도중 저장
    Former-commit-id: da6e05aa03eed11662d6c3b05086f4a36a7c5caa

--- backend/nhdream/src/main/java/com/ssafy/nhdream/common/sse/service/SseService.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/common/sse/service/SseService.java
@@ -1,4 +1,10 @@
 package com.ssafy.nhdream.common.sse.service;
 
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
 public interface SseService {
+
+    SseEmitter subscribe (String userId, String lastEventId);
+    void sendTest(String userId);
+
 }
