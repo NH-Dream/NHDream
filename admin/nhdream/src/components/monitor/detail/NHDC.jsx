89392aae1c666df3433808f79c0402ddd44e7bf3
권현준 <bonheur9813@naver.com>
2024-05-13 02:52:53 +0900

    💄 : NHDC 거래량 UI 구현
    Related to : S10P31S209-235 #done
    
    
    Former-commit-id: b0cf22d6e06996b3468217e46495742eddc1226e

--- admin/nhdream/src/components/monitor/detail/NHDC.jsx
+++ admin/nhdream/src/components/monitor/detail/NHDC.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db1987843116a9e7ef5eee51095187329c5f8caadf717a894b84627865722dbd
-size 2144
+oid sha256:c5d40f481f2fd8e63a9735c206dbe2934b4b83f74eb1984fe203c586cf1a7c8b
+size 3483
