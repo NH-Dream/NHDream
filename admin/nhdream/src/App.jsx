39774159196551bf347b7c78591a881ad4f54de0
권현준 <bonheur9813@naver.com>
2024-05-13 03:33:07 +0900

    💄 : 관리자 당행 전자지갑 관리 내역 조회
    Related to : S10P31S209-236 #done
    
    
    Former-commit-id: 50fea66958488c8f2aba9d17537a63a79c4c2e39

--- admin/nhdream/src/App.jsx
+++ admin/nhdream/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d56f9362b1231ff11ea3be0523444e9015406d355dac97c6f887f9d95dcddc2f
-size 1709
+oid sha256:38f16af331dcf094ae65ad738ce5902273af76cda740c377eecd92e380d49d67
+size 1863
--- admin/nhdream/src/pages/wallet/WalletPage.jsx
+++ admin/nhdream/src/pages/wallet/WalletPage.jsx
@@ -1,15 +1,3 @@
-import Header from "@components/@common/Header/Header";
-import SideBar from "@components/@common/SideBar/SideBar";
-
-const WalletPage = () => {
-    return (
-        <div className="flex w-screen h-screen">
-            <SideBar/>
-            <div className="flex-1 h-full bg-slate-500">
-                <Header></Header>
-            </div>
-        </div>
-    );
-};
-
-export default WalletPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:ccfc5772190db6a4f45fb14de1744036c3995a3c085565f1368e3f8102d61b25
+size 519
