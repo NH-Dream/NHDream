489a3df9d76c1ca7535aa521324a156b225750ee
권현준 <bonheur9813@naver.com>
2024-05-12 21:40:42 +0900

    💄 : 대출 심사 목록 UI 구현
    Related to : S10P31S209-230 #done
    
    
    Former-commit-id: ca77143f207c059fe954ad6202f2d115254237d3

--- admin/nhdream/src/components/@common/NavigatorLink/NavigatorLink.jsx
+++ admin/nhdream/src/components/@common/NavigatorLink/NavigatorLink.jsx
@@ -1,15 +1,3 @@
-import { useNavigate } from 'react-router-dom';
-
-const NavigatorLink = ({label, link}) => {
-    const navigate = useNavigate();
-
-    return(
-        <div>
-            <div onClick={() => navigate(link)}>
-                {label}
-            </div>
-        </div>
-    )
-}
-
-export default NavigatorLink;
+version https://git-lfs.github.com/spec/v1
+oid sha256:31b00adcf392da06baaff20840d9326d77e86093c0a64223988149415703a0ed
+size 543
