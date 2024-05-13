b8efcb24f4612486bab6769eda68309fd5e63c1d
장세정 <wkdtpwjd29@naver.com>
2024-05-13 21:40:54 +0900

    🔨 : 계좌번호-> 지갑주소수정
    Former-commit-id: c510185a1235cd843c87462b314de09c53e175c5

--- frontend/nh-dream/src/components/transfer/auto1.jsx
+++ frontend/nh-dream/src/components/transfer/auto1.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b81cd63cb6ecc3a1bcf5e65343816f049f36f0a4b58e5d7b1d6d5b6bde23bdc
+oid sha256:de7edf52eb2c4f7d3ba84057ed6300691cfe31e659a12ba3c8350a6164b7f28f
 size 4921
