c96b632ad818f0fcdfe5a03414a3a77cd3725767
조현제 <abb6575@naver.com>
2024-05-13 09:48:55 +0900

    🔨 : 엔티티 소숫점  수정
    Former-commit-id: d6a02c8cdb3b19df755657a75236c2149b1fdc16

--- backend/nhdream/src/main/java/com/ssafy/nhdream/entity/redeposit/ReDepositOptions.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/entity/redeposit/ReDepositOptions.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02da0b2fa55ae368ef7cef64d8b633b80706208a2d89c8b2e224bae718b9e24e
-size 1501
+oid sha256:e861a1594b57d337d060af95bdb83bc32d19f74fdb9376a679bad22b5fcdbd86
+size 1579
