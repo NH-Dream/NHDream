d26e8968731960e140c7dbb034cd0f92da06c473
권현준 <bonheur9813@naver.com>
2024-05-13 12:59:03 +0900

    ✨ : 정기예금 만기 기능 구현
    - 정기 예금 만기 기능 구현
    - 정기예금계좌 isActive 데이터형 변환(int -> boolean)
    
    Related to : S10P31S209-237 #done
    
    
    Former-commit-id: a191f503918db07eb91de48ccd126eeaae00e8a4

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/repository/ReDepositAccountRepository.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/repository/ReDepositAccountRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44d4f4cef5891658aa2afa9a92148a86f11f0f87084ebefeffc3aa4ec89f9616
-size 998
+oid sha256:7d50ba38b6a6652d504b3d3786e6f09f0b6562d4e40fc8df97629b9a6b5f2da4
+size 1228
