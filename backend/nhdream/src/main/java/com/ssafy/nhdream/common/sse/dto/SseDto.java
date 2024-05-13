628192394a52ef4fc61ffe8956509768099d6006
김준엽 <wnsduq1025@gmail.com>
2024-05-13 10:31:58 +0900

    ✨: sse 기븡 추가
    Former-commit-id: c3883520159cec7b2142e2e92df38dcde6cfd266

--- backend/nhdream/src/main/java/com/ssafy/nhdream/common/sse/dto/SseDto.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/common/sse/dto/SseDto.java
@@ -1,4 +1,3 @@
-package com.ssafy.nhdream.common.sse.dto;
-
-public class SseDto {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:80183383319a9406f5f8839fc34687b1647dfd368913cfb404b0507a91ae9c43
+size 2456
