bd08008988b63c03322b11abe8b9e2d5dc470cb9
이정민 <bona3377@naver.com>
2024-05-13 17:38:44 +0900

    ✨ : 새비밀번호, 지갑비밀번호 유효성 검사 기능 추가
    - 비밀번호 변경 시 유효성 검사 적용
    - 지갑비밀번호 변경 시 유효성 검사 적용
    
    
    Former-commit-id: 8ce1bfd1a4c5669f3ca4c8f0ccac174906ae8e23

--- frontend/nh-dream/src/services/wallet.js
+++ frontend/nh-dream/src/services/wallet.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5dc4c7b2c27a415a93f393b03766297948cb9770fa89dcefbb5d99cff08f93ce
-size 792
+oid sha256:a3c3b32b7b7d3e8578c75f8279c27327eb9f619a6174b7668e7da9c34620b802
+size 1249
