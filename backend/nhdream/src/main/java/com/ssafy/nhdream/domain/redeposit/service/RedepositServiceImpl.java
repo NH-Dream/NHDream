2f991ec3394555ec2e7806bf32187450660d63d0
권현준 <bonheur9813@naver.com>
2024-05-14 08:19:19 +0900

    🐛 : 예금 생성 오류 처리
    - 정기예금 계좌에 잔액 2배 되는거 수정
    - 정기예금 계좌에 Contract Address 수정
    
    
    Former-commit-id: d11cecb5c40a7e6a5a6103b7f68b0730f14a5e7b

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/service/RedepositServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/redeposit/service/RedepositServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdf9bea2eb3c3ce04f31453b86824a0cb65d16614c9579f857305ebe1460ae35
-size 20180
+oid sha256:17c00366892f1fece47d5e663401e6e84377b4aa490c89b0c8770524a15d43fb
+size 20478
