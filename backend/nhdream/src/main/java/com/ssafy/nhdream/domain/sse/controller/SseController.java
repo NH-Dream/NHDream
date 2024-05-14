963089b835d704e80ca3ebd771fede2b127afbc6
김준엽 <1025jy@naver.com>
2024-05-14 23:26:11 +0900

    🔨: sse subscribe 수정
    Former-commit-id: b4de730220348efc6ba1bb28caa01493a891fbd2

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/sse/controller/SseController.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/sse/controller/SseController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9a60a5e56e4162c64328700c809985e15e23150c31f7df4acee2d6c8f0c8383
-size 1867
+oid sha256:3ad8db23c4f41a2fe7d1af02b36fbf5407538a60c6684fc1bd2e543f30c82870
+size 1844
