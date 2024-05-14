da44d9066f499bf679ce2f566d66cad84147cf35
권현준 <bonheur9813@naver.com>
2024-05-14 23:17:39 +0900

    🔨 : 관리자페이지 CORS 설정
    Former-commit-id: 83a26b737a0aaf2571561db44c7aecfffea7e3b3

--- backend/nhdream/src/main/java/com/ssafy/nhdream/common/config/WebMvcConfig.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/common/config/WebMvcConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4f28a6be5e0a16004ac4f69c1a3a38c9bd51970c451c2617c87fdb6d715a58f
-size 824
+oid sha256:687f80039183dd64a858d25183f24f55c7a69460aa7a76f1b4566f9ed32da5bd
+size 834
