64f44c2b05a40743383d27b99a6cc28529e10149
장세정 <wkdtpwjd29@naver.com>
2024-05-14 10:05:09 +0900

    🔨 : 1차피드백수정
    Former-commit-id: 3385c568ce5942250c0975d589d4c05e867ee5de

--- frontend/nh-dream/src/components/shop/biryoList.jsx
+++ frontend/nh-dream/src/components/shop/biryoList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:161ddacc3d96904357e6f039135976e67b9f46e7230307a70edb277b457c2f58
-size 1390
+oid sha256:4cab3dae63b0fd79305ba75941e915e978528c1c0feec9f5e644b914d5a4041d
+size 1665
