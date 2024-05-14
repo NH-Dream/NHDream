1a9318b1b84e706a73f305f5210dfa3a9d81dc07
이정민 <bona3377@naver.com>
2024-05-14 13:06:07 +0900

    🐛 : access token 이슈 수정
    Former-commit-id: fed88a52a62d43c28faaf6df697445ef5540282b

--- frontend/nh-dream/src/pages/private/privateRoute.jsx
+++ frontend/nh-dream/src/pages/private/privateRoute.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+// import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { userStore } from '../../stores/userStore';
 
 const PrivateRoute = () => {
-  const { isLoggedIn } = userStore();
+  // const { isLoggedIn } = userStore();
+  const store = userStore()
 
-  if (!isLoggedIn) {
+  const isAccess = localStorage.getItem("access")
+
+  if (!isAccess) {
+    store.logout()
     return <Navigate to="/login" />;
   }
 
