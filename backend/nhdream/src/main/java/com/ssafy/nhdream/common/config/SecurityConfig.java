dfb3ba4a570fdbfd3a981060db61387465e675fb
권현준 <bonheur9813@naver.com>
2024-05-15 14:38:59 +0900

    🔨 : 관리자 프론트 CORS 처리
    - 관리자 프론트 https -> http 로 변경
    
    
    Former-commit-id: 0956dede81801520bb8fbd6a449449fdf1f41e2d

--- backend/nhdream/src/main/java/com/ssafy/nhdream/common/config/SecurityConfig.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ef436edfd40aa4d887aaf035a33e1a1b1f91a14a3650f1486c06acd627832a5
-size 4443
+oid sha256:b6736ad42e2f791a351e05ba23f3c22ea57ca709aa7e0180775803ed726144a3
+size 4442
