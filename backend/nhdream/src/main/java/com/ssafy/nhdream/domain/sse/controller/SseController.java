940ba991056089e434885f8d9867fdbe52e88838
김준엽 <wnsduq1025@gmail.com>
2024-05-15 19:45:41 +0900

    🔨: sse send 수정
    Former-commit-id: 371ce27072236b5fc45dbe16ae3a6cab155700f0

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/sse/controller/SseController.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/sse/controller/SseController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69cc31f15855fdd387310da9837f8c3ba902a5f4e1889bb3f781df5ac737c26a
-size 2523
+oid sha256:78612578e4aa902366060e0ea7544f48dc0cc61fd4fec80f40d9f0e2560faf3e
+size 2731
