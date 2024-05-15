9890cc7e1c6f705944ab7e0442e5711047d4d3b9
조현제 <abb6575@naver.com>
2024-05-15 20:44:33 +0900

    ✨ : 새관리자 기간별 토큰 소각량 발급량 조회 api 추가
    Former-commit-id: cadd9bcf2eba05738f03f76766feea3f349ab2a4

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/admin/controller/AdminController.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/admin/controller/AdminController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1ef620cc51d2846c21d14b0db221cf8a1d38f408b6159a32463fa08c3c14634
-size 4548
+oid sha256:65aa8f5ce4ccea530855674e80b5c6221c3797f8763196c39bcc58c27a26ea00
+size 5087
