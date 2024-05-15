b1cea96e913aed257a43361387afdfaf5d79993f
이정민 <bona3377@naver.com>
2024-05-16 02:30:08 +0900

    ✨ : 새블록체인 불러오기, DRDC 토큰 차트화 기능 추가
    Former-commit-id: b2ac40dca2a53adcea35295fccb9bb626121c98d

--- admin/nhdream/src/components/monitor/detail/DRDC.jsx
+++ admin/nhdream/src/components/monitor/detail/DRDC.jsx
@@ -1,12 +1,3 @@
-
-
-const DRDC = () => {
-
-    return(
-        <div className="w-full h-full bg-CONTENT">
-            DRDC
-        </div>
-    )
-}
-
-export default DRDC; 
+version https://git-lfs.github.com/spec/v1
+oid sha256:6aa9a9e35b08cd2a719f82089b464ed663ef27afcaaf26339d45fcf43a37c405
+size 10483
