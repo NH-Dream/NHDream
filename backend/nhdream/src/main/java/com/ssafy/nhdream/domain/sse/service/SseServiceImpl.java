7d283303376180dea4fd5832d2763ec596a185d2
김준엽 <wnsduq1025@gmail.com>
2024-05-15 17:54:27 +0900

    ✨: notification test 추가
    Former-commit-id: 38c25073fb1beafd5048d4cff103cadd51b739cf

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/sse/service/SseServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/sse/service/SseServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6213359110936841e381a457b409e0882bcbe6b6e566b5bc21384d0e37fc297d
-size 4953
+oid sha256:cc9217f1e2cfe8b08a9a6f65d74fb6eef5ae662f11014be4b62a6c0731d06563
+size 5351
