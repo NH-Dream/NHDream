2012976de04fe4b6959701ffd88e0260bafd9785
김준엽 <1025jy@naver.com>
2024-05-15 16:10:27 +0900

    🔨: saveNotification 수정
    Former-commit-id: 31a5e992d503266c0d677983636e04b3c0d43430

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/sse/service/SseServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/sse/service/SseServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:633e3e139392e283cbda429e984627610caf6dcee9afb8b158d906fce338b463
+oid sha256:6213359110936841e381a457b409e0882bcbe6b6e566b5bc21384d0e37fc297d
 size 4953
