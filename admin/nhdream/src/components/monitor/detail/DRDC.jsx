dfcdbc249099429f54ca5668574f9351a85e9b5d
이정민 <bona3377@naver.com>
2024-05-16 02:56:12 +0900

    🚧 : 작업 도중 저장
    Former-commit-id: c2ce2274ad5725950146a958a3f5310d9c144ba6

--- admin/nhdream/src/components/monitor/detail/DRDC.jsx
+++ admin/nhdream/src/components/monitor/detail/DRDC.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:277f196895e33ddf1cf713f4c7b6045c0763268feeba6f416a3f9d38b8224a31
-size 10491
+oid sha256:b9a428526809392d8c1f520d311ed789cbad016ba14724ca11a8478d7046ca45
+size 11724
