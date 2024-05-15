d4aa0897a446092954b2ffa729164878d290589c
권현준 <bonheur9813@naver.com>
2024-05-15 21:06:23 +0900

    🔨 : 각 항목 순서 전달 및 전체 페이지 전달
    Former-commit-id: b62bc2c44671ae70f212f16a762b1df875099b1f

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/admin/service/AdminServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/admin/service/AdminServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09c9faedc9d7f49ad70eed16b8a5593f445fca12abb43d2ecf89b8cc7d970e98
-size 9959
+oid sha256:b85af3683cc66bdeb6826f8c45817cadfa9128870e41139cd937c2883fe74b88
+size 10146
