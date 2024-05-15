8646dc8002a42ddba7666965bcb71d607d831e34
김준엽 <wnsduq1025@gmail.com>
2024-05-15 16:10:27 +0900

    🔨: saveNotification 수정
    Former-commit-id: 31a5e992d503266c0d677983636e04b3c0d43430

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/sse/controller/SseController.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/sse/controller/SseController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f6d5f82e7821ee3ad9099aba842975e4b7df1bf4e24d1b0e58dacfbbc77e0eb
-size 2027
+oid sha256:549ab48cd35e623e6f1fd20609793845762cface7f4c8bc0a68c4164e1a8b593
+size 2042
--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/sse/service/NotificationServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/sse/service/NotificationServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad8ad55ee5907f43c5b4b99bc3c8f40ec9deaa1889671e7d118142be06943d3b
-size 3820
+oid sha256:3b56b795429bae7f35aa01dc10aa764932de2221b582b57a1580ada37b1af7d8
+size 4017
