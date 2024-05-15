e1b36b70af6cbc1084d51852694c5e5029c73977
권현준 <bonheur9813@naver.com>
2024-05-15 20:07:50 +0900

    ✨ : 대출 심사 API 연결
    Related to : S10P31S209-245 #done
    
    
    Former-commit-id: 8bdaa99c8ca23826f376442255403d0939ba911c

--- admin/nhdream/src/services/review.js
+++ admin/nhdream/src/services/review.js
@@ -1,18 +1,3 @@
-import api from '@/services/api'
-
-function getLoanReviewList(success, fail) {
-    api.get('/admins/review/loan')
-    .then(response => success(response.data.dataBody))
-    .catch(error => fail(error))
-}
-
-function getLoanReviewDetail(loanReviewId, success, fail) {
-    api.get(`/admins/review/loan/${loanReviewId}`)
-    .then(response => success(response.data.dataBody))
-    .catch(error => fail(error))
-}
-
-export {
-    getLoanReviewList, 
-    getLoanReviewDetail,
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:102df18bd91f1856319ce235313ec2a710f3684f5279c125727ff37d2f24dff1
+size 745
