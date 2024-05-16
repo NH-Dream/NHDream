8305c718e34a8c6097296628a37c23106a0c15fb
권현준 <bonheur9813@naver.com>
2024-05-17 05:34:01 +0900

    🐛 : 이름 검색시 데이터 랜더링 수정
    - 이름 검색시 페이지를 설정된페이지가 아닌 시작페이지( 1)로 변경
    
    
    Former-commit-id: 3f7c44f24d040b05f338c5b85c8c2ca145e920aa

--- admin/nhdream/src/services/review.js
+++ admin/nhdream/src/services/review.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c245ef1bbe37696e30beed5bfae64ad6f1bd1f16767732191a06cb3e17534768
+oid sha256:3bc370a9c1f2894f9fc7b421b134bce51e41833c1b38e9bbfe5d888e08579eae
 size 1677
