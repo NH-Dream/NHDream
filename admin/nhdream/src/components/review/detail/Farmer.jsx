913e138d8700d4ac9c28c942da802106e97601f3
권현준 <bonheur9813@naver.com>
2024-05-17 05:34:01 +0900

    🐛 : 이름 검색시 데이터 랜더링 수정
    - 이름 검색시 페이지를 설정된페이지가 아닌 시작페이지( 1)로 변경
    
    
    Former-commit-id: 3f7c44f24d040b05f338c5b85c8c2ca145e920aa

--- admin/nhdream/src/components/review/detail/Farmer.jsx
+++ admin/nhdream/src/components/review/detail/Farmer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afc47d1aae147d3cd34c135fc5e62341520ce69d6b1c923599af2178925947a9
-size 5602
+oid sha256:5880d0e5a71468ab447ea12b422d1ff2f0034ece1d3c4c7defad3a449929aba0
+size 5592
--- admin/nhdream/src/components/review/detail/Training.jsx
+++ admin/nhdream/src/components/review/detail/Training.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:939b654577a8e3ec99fe8c0112842a4f65e8d978eec5e528d3d5983b50b3848a
-size 9381
+oid sha256:ecda9509b881bbe8b24ef42168d0b4b87c239548fa355daa87efe94fc2c45f78
+size 9371
