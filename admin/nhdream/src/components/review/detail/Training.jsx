b7dd23f3cf1171d2e9399ad4b98e3727c6ba6d91
권현준 <bonheur9813@naver.com>
2024-05-17 03:37:43 +0900

    ✨ : 관리자 교육생 심사 목록 조회 API 구현
    Related to : S10P31S209-257 #done
    
    
    Former-commit-id: 69d4385b7bb6e1d19849d2f6ddfec1a5fbf61a0f

--- admin/nhdream/src/components/review/detail/Training.jsx
+++ admin/nhdream/src/components/review/detail/Training.jsx
@@ -1,12 +1,3 @@
-
-
-const Training = () => {
-
-    return(
-        <div className="w-full h-full bg-CONTENT">
-            TrainingReview
-        </div>
-    )
-}
-
-export default Training; 
+version https://git-lfs.github.com/spec/v1
+oid sha256:939b654577a8e3ec99fe8c0112842a4f65e8d978eec5e528d3d5983b50b3848a
+size 9381
---
+++ admin/nhdream/src/components/review/detail/TrainingData.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:996c144c352ff3e7a18cb0bc1aa2710eb01e63c716536bee7a05c0f58ea490a0
+size 875
