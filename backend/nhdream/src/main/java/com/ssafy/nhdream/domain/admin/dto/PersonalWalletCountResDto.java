e6d0803854dc673be1dcba20a9e1f71ba2990d0b
조현제 <abb6575@naver.com>
2024-05-16 09:44:58 +0900

    ✨ : 개인지갑 카운트  기능 추가
    Former-commit-id: 90420776002dee5cb4629deb4cc53aa2159a307c

---
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/admin/dto/PersonalWalletCountResDto.java
@@ -0,0 +1,18 @@
+package com.ssafy.nhdream.domain.admin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigInteger;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class PersonalWalletCountResDto {
+    private int count;
+
+
+}
--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/admin/service/AdminServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/admin/service/AdminServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d3146f11a59dd01dd957db1e7378fdc950f93d91cf317f97ea44500bb881ad5
-size 12270
+oid sha256:6aa5bce01f4f0bbea855212c32ca096958f71cdfa7085c8aae35ed77b4a6c3e9
+size 12444
