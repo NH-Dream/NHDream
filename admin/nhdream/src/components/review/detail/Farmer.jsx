754addc806d1397b324ad3c451d812288b2f50e6
권현준 <bonheur9813@naver.com>
2024-05-17 04:25:33 +0900

    ✨ : 관리자 귀농 심사 목록 조회 API 연결
    Related to : S10P31S209-259 #done
    
    
    Former-commit-id: 35f8421246676c8037a30ec07d6132c709e46078

--- admin/nhdream/src/components/review/detail/Farmer.jsx
+++ admin/nhdream/src/components/review/detail/Farmer.jsx
@@ -1,12 +1,3 @@
-
-
-const Farmer = () => {
-
-    return(
-        <div className="w-full h-full bg-CONTENT">
-            FarmerReview
-        </div>
-    )
-}
-
-export default Farmer; 
+version https://git-lfs.github.com/spec/v1
+oid sha256:6e3ad1bd6e99f81989d02fbb3de3e8dddb6fe57dc8b67e5bc81ccd6b33c90ffc
+size 5029
---
+++ admin/nhdream/src/components/review/detail/FarmerData.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:939c7b6558e25c0bf7972530e70012ed36b3ae28633e25b8d1af1163b0af1858
+size 869
