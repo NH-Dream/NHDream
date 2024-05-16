1d0743e14469f9da93b0e1b50f6ef390863be09c
권현준 <bonheur9813@naver.com>
2024-05-16 12:33:36 +0900

    💄 : 대출 심사 후 상태 바로 반영 및 에러 모달 처리
    Former-commit-id: f2b35f6b8e23de4b31d357dff2342cf7a06d006d

--- admin/nhdream/src/components/review/detail/Loan.jsx
+++ admin/nhdream/src/components/review/detail/Loan.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dacc14ec9419093d498b0f14a46cd702801e5e3db27e03a2a79e02c1c86049d4
-size 4935
+oid sha256:970b486f621725f5fe098f0882e3282e1573c367e1c44a24366a7ece0cbd31b2
+size 5444
