8a5d1070bd5eb355786246590ecd1ecd198de65f
권현준 <bonheur9813@naver.com>
2024-05-17 18:02:38 +0900

    💄 : 사이드바 클릭 시 폰트색 변경
    Former-commit-id: af913b59428c1bbff44d9ab332ca62191987c8f7

--- admin/nhdream/src/stores/navStore.js
+++ admin/nhdream/src/stores/navStore.js
@@ -4,7 +4,9 @@ import {persist} from "zustand/middleware";
 const navStore = create(
   persist(
     (set) => ({
+      curSideBar: '', 
       curPosition: '',
+      setCurSideBar: (curSideBar) => set({ curSideBar }),
       setCurPosition: (curPosition) => set({ curPosition }),
     }),
     {
