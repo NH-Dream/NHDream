7ca048a7089c0ed2eded7f143540ec4ad8132bba
장세정 <wkdtpwjd19@gmail.com>
2024-05-17 20:44:05 +0900

    🔨 : 디자인 수정
    Former-commit-id: 30e47d8c1162328785a258fb1b297246ee08e538

--- frontend/nh-dream/src/components/mypage/menuItems.jsx
+++ frontend/nh-dream/src/components/mypage/menuItems.jsx
@@ -2,8 +2,8 @@ export default function MenuItems({ icon, title, onClick }){
   const isAutoTransfer = icon === 'AutoTransfer'
   
   return(
-    <div className="p-3 pl-3 shadow-md rounded-xl flex h-20" onClick={onClick}>
-      <div className="flex items-center"><img src={icon} alt={title} className="mt-2" style={{ width:33,height:30}}/></div>
+    <div className="p-3  shadow-md rounded-xl flex h-20" onClick={onClick}>
+      <div className="flex items-center"><img src={icon} alt={title} className="" style={{ width:35,height:35}}/></div>
       <div className="flex items-center text-lg font-bold ml-5">{title}</div>
     </div>
   )
