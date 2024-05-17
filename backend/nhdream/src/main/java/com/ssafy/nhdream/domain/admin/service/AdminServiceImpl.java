c9cb1697e4508b79888c5f69374bfd7730a2a7b2
권현준 <bonheur9813@naver.com>
2024-05-17 15:04:22 +0900

    🔨 : 교육 인증 로직 수정
    - 교육 인증 로직 수정
    
    
    Former-commit-id: 4727b709f4f3d3126ca70c503045f6bd7ddf3e0b

--- backend/nhdream/src/main/java/com/ssafy/nhdream/domain/admin/service/AdminServiceImpl.java
+++ backend/nhdream/src/main/java/com/ssafy/nhdream/domain/admin/service/AdminServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96523f8bfef576f0d26796bac67d6fc28d2722c319e6e3dedda498b1c21d7b39
-size 15846
+oid sha256:50cc417460d8ec02a9c50e95e85a599316fb808d5267f048a20ec6d0f6bea2a2
+size 15803
