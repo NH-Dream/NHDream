67714cba6d2acf6081a2c72143810c6f3344c66b
이정민 <bona3377@naver.com>
2024-05-17 11:34:31 +0900

    🔨 : 지갑 불러오는 코드 수정
    Former-commit-id: fedd58ced99e737973e37a71b2376bbd25c595a7

--- admin/nhdream/src/components/wallet/detail/Wallet.jsx
+++ admin/nhdream/src/components/wallet/detail/Wallet.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0184027aa8017d318b9c23f3dafa7b3021bce74c25de0c67ce4d3d83f113c307
-size 2919
+oid sha256:39221ad4b990dcabb1f343786a833fbfb2d78da990e155a5c108b9829c45778d
+size 4028
