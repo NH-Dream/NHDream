da203acedcd92f114f4213f27c3d31a36dcc3537
장세정 <wkdtpwjd19@gmail.com>
2024-05-18 22:53:17 +0900

    🔨 : 코드 수정
    Former-commit-id: c41a4b58d58891961fceb0acd6f40090ba49d72d

--- frontend/nh-dream/src/components/transfer/auto1.jsx
+++ frontend/nh-dream/src/components/transfer/auto1.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac5c03a8b2bb5c958c8b8fb41f1f8e9b88c6dbf8d85d0b33f41b635c6cbfe4d3
-size 7371
+oid sha256:fd870423d2e888eeaa1d964e47a36790dc71607f245ea1a3c2a3aace8001a9ca
+size 7388
