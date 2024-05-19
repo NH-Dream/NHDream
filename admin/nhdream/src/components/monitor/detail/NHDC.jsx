9a7d7be5e04e3c7735a69d7c08c01aadbed815f9
권현준 <bonheur9813@naver.com>
2024-05-19 20:09:11 +0900

    🔨 : 그래프 범위 수정
    Former-commit-id: 54880d76f5668ffbb140378d52cf62571c1c1c8b

--- admin/nhdream/src/components/monitor/detail/NHDC.jsx
+++ admin/nhdream/src/components/monitor/detail/NHDC.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3762de21319699b61d2959d1a00aab73ae7dfa38c13ecde86dbc8e1ef7a6a46f
+oid sha256:2b71cca2779723941b55e5314861e423c51be6e8bb16d114140b816cc3ff04f5
 size 14200
