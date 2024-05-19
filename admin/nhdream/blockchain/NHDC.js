a90b4bc8959ae5b6e924dad1f48d13a252eb6b49
이정민 <bona3377@naver.com>
2024-05-19 21:13:20 +0900

    Update admin/nhdream/blockchain/NHDC.js, admin/nhdream/src/components/wallet/detail/Wallet.jsx
    Former-commit-id: 51072ebe526b1c23ff962fa5034a92aeb5e9d5e8

--- admin/nhdream/blockchain/NHDC.js
+++ admin/nhdream/blockchain/NHDC.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ec1f9237cb0bed92e49911b1442630612b2c7518a3a54a4e8931794f6a6d934
-size 3670
+oid sha256:4e43feb08a46dcf8eeda0ff7f159b94b90ef8fce2ba606d6c319d4c64a110ff0
+size 3671
