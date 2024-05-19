f673da14cfa51fd1d6eb317e930cc864a2f6e435
조현제 <abb6575@naver.com>
2024-05-19 21:29:57 +0900

    🔨 : 필드명 수정
    Former-commit-id: 0f22f31d222c7ef222a7e87e0d345cb35d59de4a

--- admin/nhdream/blockchain/NHDC.js
+++ admin/nhdream/blockchain/NHDC.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ec1f9237cb0bed92e49911b1442630612b2c7518a3a54a4e8931794f6a6d934
+oid sha256:f4884356a4aa0fc7f832a535d0c6ec857d73df400fc00e39f6a4a09454ee2759
 size 3670
--- admin/nhdream/src/components/wallet/detail/Wallet.jsx
+++ admin/nhdream/src/components/wallet/detail/Wallet.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c432783bb01a6a74990a076f9079b5657b87fd010c4fc8f758d2ee990f88d31
-size 4027
+oid sha256:a4018a12a6e1e2aa8216365b708367f51edfd0aaf617e00733922e66de8b7c08
+size 4028
